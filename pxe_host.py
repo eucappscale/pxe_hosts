79c5688a8c608c1d22ee0d4f0788b99ce96c4e00
eucappscale <34253624+eucappscale@users.noreply.github.com>
2017-12-04 15:08:37 -0800

    Create pxe_host.py

---
+++ pxe_host.py
@@ -0,0 +1,31 @@
+"""
+This script will execute koan -r and the necessary reboot to start the PXE boot
+process on a target host.
+
+required params:
+  user - user to login as 
+  password - login users password
+  target - target host to run on
+
+usage:
+  python pxe_host.py -u root -p foobar -t 192.168.0.12
+  
+"""
+import os
+import argparse
+import paramiko
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--user", help="user to log in as")
+parser.add_argument("-p", "--password", help="user password")
+parser.add_argument("-t", "--target", help="target host")
+args = parser.parse_args()
+
+user = args.user
+password = args.password
+host = args.target
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(str(host), username=user, password=password)
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("touch remote_test")
