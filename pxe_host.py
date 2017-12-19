5d39b238a1c621c8f344e6e213f0002bc529b398
eucappscale <34253624+eucappscale@users.noreply.github.com>
2017-12-19 10:07:03 -0800

    Update pxe_host.py
    update to take comma separated list of hosts to pxe

--- pxe_host.py
+++ pxe_host.py
@@ -8,7 +8,7 @@ required params:
   target - target host to run on
 
 usage:
-  python pxe_host.py -u root -p foobar -t 192.168.0.12
+  python pxe_host.py -u root -p foobar -t 192.168.0.12,192.168.0.13
   
 """
 import os
@@ -18,14 +18,16 @@ import paramiko
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--user", help="user to log in as")
 parser.add_argument("-p", "--password", help="user password")
-parser.add_argument("-t", "--target", help="target host")
+parser.add_argument("-t", "--target", help="comma separated list of target hosts")
 args = parser.parse_args()
 
 user = args.user
 password = args.password
-host = args.target
+host_list = args.target.split(',')
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(str(host), username=user, password=password)
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("yum install -y koan python-ethtool; koan -r; reboot")
+for host in host_list:
+    print "PXEing host " + host
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(str(host), username=user, password=password)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("yum install -y koan python-ethtool; koan -r; reboot")
